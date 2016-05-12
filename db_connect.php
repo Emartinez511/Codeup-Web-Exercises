34772517400ba2a9718e983c32780bd74c6eb1ca
Emartinez511 <mrblonde9993@gmail.com>
2016-05-12 10:13:31 -0500

    removed creds from db.

--- db_connect.php
+++ db_connect.php
@@ -1,7 +1,8 @@
 
 <?php
+
 // Get new instance of PDO object
-$dbc = new PDO('mysql:host=127.0.0.1;dbname=employees', 'codeup', 'helloworld');
+$dbc = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME, DB_USER, DB_PASS);
 
 // Tell PDO to throw exceptions on error
 $dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
